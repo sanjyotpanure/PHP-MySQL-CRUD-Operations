a3fc356abf43eb5c06e7a9a0d3ff1afe0835f21a
Sanjyot Panure <69676402+sanjyotpanure@users.noreply.github.com>
2022-04-14 10:10:54 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $user = 'root';
     $password = '';
-    $database = 'i2it';
+    $database = 'projects';
 
     $con = mysqli_connect($host, $user, $password, $database);
 
